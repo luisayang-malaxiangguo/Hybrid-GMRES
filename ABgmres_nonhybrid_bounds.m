c7afee8293a6bc96a97f10fee5cb43bda39c628e
luisayang-malaxiangguo <luisa.yang@univ.ox.ac.uk>
2025-08-29 17:17:18 +0200

    Update ABgmres_nonhybrid_bounds.m

--- ABgmres_nonhybrid_bounds.m
+++ ABgmres_nonhybrid_bounds.m
@@ -1,16 +1,13 @@
 function [x, err, res, niters, phi_final, dphi_final, phi_iter, dphi_iter] = ABgmres_nonhybrid_bounds( ...
     A, B, b, x_true, tol, maxit, DeltaM)
     
-    % Build M = A*B and get its eigenvalues/vectors for analysis
     M = A * B;
     m = size(A, 1);
     
-    % The eigenvalues 'mu' mre computed from the actual system matrix M = A*B.
     [U_M, D_M] = eig(M);
     mu_full = real(diag(D_M));
     % Sorting for consistent mode analysis
     [mu_full, sort_idx] = sort(mu_full, 'descend');
-    % matrix UA stores the eigenvectors of M, needed for dMu calculation
     UA = U_M(:, sort_idx);
 
     %--- Arnoldi GMRES on M z = b ---%    
@@ -23,7 +20,6 @@ function [x, err, res, niters, phi_final, dphi_final, phi_iter, dphi_iter] = ABg
     res   = zeros(maxit,1);
     err   = zeros(maxit,1);
     
-    % Initialize to store filter factors and perturbation bounds
     phi_iter = cell(maxit, 1);
     dphi_iter = cell(maxit, 1);
 
@@ -44,7 +40,6 @@ function [x, err, res, niters, phi_final, dphi_final, phi_iter, dphi_iter] = ABg
         z  = Q(:,1:k) * yk;
         xk = B * z;
         
-        % Compute norms & check convergence
         res(k) = norm(b - A*xk)/norm(b);
         err(k) = norm(xk - x_true)/norm(x_true);
         
@@ -52,7 +47,6 @@ function [x, err, res, niters, phi_final, dphi_final, phi_iter, dphi_iter] = ABg
         % Compute harmonic-Ritz values and their perturbations
         Qk_current = Q(:,1:k);
         Hk_small_current = H(1:k,1:k);
-        % The projected perturbation is Qk'*DeltaM*Qk
         dKk_current = Qk_current' * DeltaM * Qk_current;
         ek_current = zeros(k,1); 
         ek_current(end) = 1;
@@ -64,16 +58,9 @@ function [x, err, res, niters, phi_final, dphi_final, phi_iter, dphi_iter] = ABg
         [Theta_current, p_sort] = sort(Theta_current); % sort ascending
         W_current = W_current(:, p_sort);   % reorder harmonic-Ritz vectors
         
-        % Perturbation of harmonic-Ritz values
         dTheta_current = real(diag(W_current' * dKk_current * W_current));
-        
-        % CORRECTED perturbation of eigenvalues using eigenvectors of M=A*B
         dMu_current = sum(UA(:,1:k) .* (DeltaM * UA(:,1:k)), 1)';
-        
-        % Truncate mu to the current size k
         mu_current = mu_full(1:k);
-        
-        % Build phi and dphi using formula for non-hybrid methods
         eps0_current = eps;
         Clog_current = zeros(k,1);
         P_excl_current = zeros(k,k);
@@ -88,12 +75,10 @@ function [x, err, res, niters, phi_final, dphi_final, phi_iter, dphi_iter] = ABg
         P_final = exp(Clog_current);
         phi_current = 1 - P_final; % Filter factors for this k
         
-        % Perturbation bound calculation for non-hybrid method (two terms)
         term1 = - mu_current .* sum((dTheta_current' ./ Theta_current'.^2) .* P_excl_current, 2);
         term2 =  sum((1./Theta_current') .* P_excl_current, 2) .* dMu_current;
         dphi_current = term1 + term2; % Perturbation bound for this k
         
-        % Store results
         phi_iter{k} = phi_current;
         dphi_iter{k} = dphi_current;
         
@@ -109,4 +94,5 @@ function [x, err, res, niters, phi_final, dphi_final, phi_iter, dphi_iter] = ABg
     
     phi_iter = phi_iter(1:k);
     dphi_iter = dphi_iter(1:k);
+
 end
