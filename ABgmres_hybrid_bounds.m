7a8971fdb370899dc03d41852b71f6085b9ddd01
luisayang-malaxiangguo <luisa.yang@univ.ox.ac.uk>
2025-08-29 17:16:02 +0200

    Update ABgmres_hybrid_bounds.m

--- ABgmres_hybrid_bounds.m
+++ ABgmres_hybrid_bounds.m
@@ -1,13 +1,8 @@
 function [x, error_norm, residual_norm, niters, phi_final, dphi_final, phi_iter, dphi_iter] = ABgmres_hybrid_bounds( ...
     A, B, b, x_true, tol, maxit, lambda, DeltaM)
-% ABGMRES_HYBRID_BOUNDS AB-GMRES with Tikhonov regularization and perturbation bounds.
-% This function returns filter factors (phi) and their perturbation bounds (dPhi) at each iteration.
 
     M = A * B;
     m = size(A, 1);
-    
-    % The eigenvalues 'mu' must be computed from the actual system matrix M = A*B.
-    % Since M may be non-symmetric, we use eig().
     [U_M, D_M] = eig(M);
     mu_full = real(diag(D_M));
     % Sort for consistent mode analysis
@@ -48,11 +43,9 @@ function [x, error_norm, residual_norm, niters, phi_final, dphi_final, phi_iter,
         zk = Q(:,1:k)*yk;
         xk = B * zk;
         
-        % Norms & stopping
         residual_norm(k) = norm(b - A*xk)/norm(b);
         error_norm(k)    = norm(xk - x_true)/norm(x_true);
         
-        % Form projected perturbation
         Qk_current = Q(:,1:k);
         % For AB-GMRES, the perturbation to M=AB is DeltaM, which is assumed to be A*E'
         dK_small_current = Qk_current' * DeltaM * Qk_current;
@@ -71,10 +64,8 @@ function [x, error_norm, residual_norm, niters, phi_final, dphi_final, phi_iter,
         dTheta_current = real(diag(W_current' * dK_small_current * W_current));
         dMu_current = sum(UA(:,1:k) .* (DeltaM * UA(:,1:k)), 1)';
         
-        % Truncate mu to the current size k
-        mu_current = mu_full(1:k); % This now uses the CORRECT eigenvalues
+        mu_current = mu_full(1:k); 
         
-        % Build phi and dphi using filter factor formula from Theorem 4.5.1
         s2l = mu_current + lambda;
         eps0_current = eps;
         Clog_current = zeros(k,1);
@@ -90,13 +81,12 @@ function [x, error_norm, residual_norm, niters, phi_final, dphi_final, phi_iter,
         P_final = exp(Clog_current);
         phi_current = (mu_current ./ s2l) .* (1 - P_final);
         
-        % HYBRID perturbation bound (3 terms)
+        % HYBRID perturbation bound 
         term1 = - (mu_current) .* sum((dTheta_current.' ./ Theta_current.'.^2) .* P_excl_current, 2);
         term2 =   (lambda ./ s2l.^2) .* (1 - P_final) .* dMu_current;
         term3 =   (mu_current ./ s2l) .* sum((1./Theta_current') .* P_excl_current, 2) .* dMu_current;
         dphi_current = term1 + term2 + term3;
         
-        % Store the calculated phi and dphi
         phi_iter{k} = phi_current;
         dphi_iter{k} = dphi_current;
 
@@ -114,3 +104,4 @@ function [x, error_norm, residual_norm, niters, phi_final, dphi_final, phi_iter,
     dphi_iter = dphi_iter(1:k);
 
 end
+
