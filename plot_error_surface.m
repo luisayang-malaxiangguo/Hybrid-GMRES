bad0e73cd36ec7a3d46c798c414bdbb674cd5f7d
luisayang-malaxiangguo <luisa.yang@univ.ox.ac.uk>
2025-08-29 17:47:36 +0200

    Update plot_error_surface.m

--- plot_error_surface.m
+++ plot_error_surface.m
@@ -2,8 +2,7 @@ function plot_error_surface()
 
 clear all;
 clc;
-
-%% 1) Set up Test Problem and Parameters
+ 
 n = 32;
 problem_name = 'deriv2';
 [A, b_exact, x_true] = generate_test_problem(problem_name, n);
@@ -24,18 +23,15 @@ lambda_range = logspace(-8, -1, 100);
  
 error_surface_ab = zeros(length(lambda_range), length(k_range));
 error_surface_ba = zeros(length(lambda_range), length(k_range));
-
-%% 2) Loop Through Parameters and Compute Errors for Both Methods 
+ 
 tic; 
 for i = 1:length(lambda_range)
     lambda = lambda_range(i);
-    
-    % Hybrid AB-GMRES 
+     
     [~, err_hist_ab, ~, ~] = ABgmres_hybrid_bounds(A, B_pert, b_noise, x_true, 1e-10, n, lambda, DeltaM_AB);
     if length(err_hist_ab) < n, err_hist_ab(end+1:n) = NaN; end
     error_surface_ab(i, :) = err_hist_ab;
-    
-    % Hybrid BA-GMRES 
+     
     [~, err_hist_ba, ~, ~] = BAgmres_hybrid_bounds(A, B_pert, b_noise, x_true, 1e-10, n, lambda, DeltaM_BA);
     if length(err_hist_ba) < n, err_hist_ba(end+1:n) = NaN; end
     error_surface_ba(i, :) = err_hist_ba;
@@ -45,10 +41,7 @@ for i = 1:length(lambda_range)
     end
 end
 toc;  
-
-%% 3) Hybrid AB-GMRES 
-
-% optimal point for AB-GMRES
+ 
 [min_val_ab, min_idx_flat_ab] = min(error_surface_ab(:));
 [min_row_ab, min_col_ab] = ind2sub(size(error_surface_ab), min_idx_flat_ab);
 optimal_k_ab = k_range(min_col_ab);
@@ -72,9 +65,7 @@ title('Error Surface for Hybrid AB-GMRES');
 legend('show', 'Location', 'NorthEast');
 grid on;
 set(gca, 'FontSize', 12);
-
-%% 4) Hybrid BA-GMRES
-%  optimal point for BA-GMRES
+ 
 [min_val_ba, min_idx_flat_ba] = min(error_surface_ba(:));
 [min_row_ba, min_col_ba] = ind2sub(size(error_surface_ba), min_idx_flat_ba);
 optimal_k_ba = k_range(min_col_ba);
@@ -99,4 +90,5 @@ grid on;
 set(gca, 'FontSize', 12);
 
 end
+
 
